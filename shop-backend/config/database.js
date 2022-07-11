f22ff28d486b9d56624cc3411551de9fe4858a32
derekhongg <hongderek@gmail.com>
2022-07-11 15:51:23 -0700

    adding files

---
+++ shop-backend/config/database.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: false,
+    },
+    debug: false,
+  },
+});
