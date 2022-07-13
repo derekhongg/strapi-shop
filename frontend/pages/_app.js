a40ae8c0977476c58215700ace6a371c7fbb29f7
derekhongg <hongderek@gmail.com>
2022-07-13 16:21:26 -0700

    added cart

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,37 @@
+import { useEffect, useState } from 'react'
 import NavBar from '../components/NavBar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <><NavBar/><Component {...pageProps} /></>
+  useEffect(() => {
+    console.log("I am useEffect from app.js")
+  }, [])
+
+  const [cart, setCart] = useState([])
+  const [reloadKey, setReloadKey] = useState(1)
+  
+  const addToCart = (item, qty, price) => {
+    let newCart = cart
+    for(let i = 0; i < qty; i++){
+      newCart.push([item, price])
+    }
+    console.log("Add to cart", newCart)
+    setCart(newCart)
+    setReloadKey(Math.random())
+  }
+
+  const removeFromCart = (item, qty) => {
+    let newCart = cart
+    let index = newCart.indexOf(item)
+    newCart.splice(index)
+    setCart(newCart)
+  }
+
+  const clearCart = (item) => {
+    setCart([])
+  }
+  
+  return <><NavBar key={reloadKey} cart={cart}/><Component cart={cart} removeFromCart={removeFromCart} addToCart={addToCart} clearCart={clearCart} {...pageProps} /></>
 }
 
 export default MyApp
